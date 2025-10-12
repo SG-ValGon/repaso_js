0ba483a0cb8867ef37d89e6dd7f700a625f0a81a
Gustavo Valencia Gonzales <sgiext2@gmail.com>
2025-10-12 13:31:18 -0600

    Actualizacion

---
+++ main.js
@@ -0,0 +1,50 @@
+var nombre = "Gustavo Valencia";
+var altura = 170;
+/*
+var concatenacion = nombre + " " + altura;
+var datos = document.getElementById("datos");
+datos.innerHTML = `
+    <h1>Hola soy la caja de datos</h1>
+    <h2>Mi nombre es: ${nombre}</h2>
+    <h3>Mido: ${altura} cm </h3>
+
+
+`;
+if (altura >= 190) {
+    datos.innerHTML += ` <h1> Eres una persona alta </h1>`;
+} else {
+    datos.innerHTML += ` <h1> Eres una persona baja </h1>`;
+}
+
+for (var i = 2000; i <= 2025; i++) {
+    //bloque de instrucciones
+    datos.innerHTML += "<h2>Estamos en el año: </h2>" + i;
+}
+*/
+function MuestraMiNombre(nombre, altura) {
+    var misdatos = `
+    <h1>Hola soy la caja de datos</h1>
+    <h2>Mi nombre es: ${nombre}</h2>
+    <h3>Mido: ${altura} cm </h3>
+    `;
+
+    return misdatos;
+}
+
+function imprimir() {
+    var datos = document.getElementById("datos");
+    datos.innerHTML = MuestraMiNombre("Gustavo Valencia", 183);
+}
+
+imprimir();
+
+var nombres = ['Gustavo', 'Ana', 'Pepe'];
+
+document.write('<h1>Listado de nombres</h1>');
+/*for (i = 0; i < nombres.length; i++) {
+    document.write(nombres[i] + '<br>');
+
+}*/
+nombres.forEach((nombre) => {
+    document.write(nombre + '<br>');
+});
