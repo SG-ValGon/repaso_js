66d68807319111d8056db0be03b663d469f2fb92
Gustavo Valencia Gonzales <sgiext2@gmail.com>
2025-10-16 23:54:02 -0600

    Actualizacion 1

--- main.js
+++ main.js
@@ -62,3 +62,22 @@ var coche = {
 document.write("<h1>" + coche.antiguedad + "</h1>");
 
 coche.mostrarDatos();
+
+var saludar = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        let saludo = "Hola a todos";
+        saludo = false;
+        if (saludo) {
+            resolve(saludo);
+        } else {
+            reject('No hay saludo disponible');
+        }
+    }, 2000);
+});
+
+saludar.then(resultado => {
+        alert(resultado);
+    })
+    .catch(err => {
+        alert(err);
+    });
